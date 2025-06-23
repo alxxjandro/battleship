e59d52fd145be5cf3b80eaabe247c04a94546505
Alonso Alarcon <155941412+alxxjandro@users.noreply.github.com>
2025-06-22 22:11:57 -0600

    modified the function that creates the board in order to check and color ships

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import Game from "./game.js";
+import Ship from "./ship.js"
 
 //This file will handle loading the GUI and making the correct visual changes
 const loadBoard = (gameboard) =>{
@@ -8,21 +9,28 @@ const loadBoard = (gameboard) =>{
   for (let rows of gameboard.board){
     let rowDiv = Object.assign(document.createElement("div"),{className: `row${rows[0].row}`});
     for (let columns of rows){
-      rowDiv.appendChild(createTile(rows[0].row,columns.column))
+      rowDiv.appendChild(createTile(rows[0].row, columns.column, columns.ship)) // columns.ship could be null!
     }
     boardContainer.appendChild(rowDiv);
   }
   return boardContainer;
 }
 
-const createTile = (row, column) =>{
+const createTile = (row, column, ship) =>{
   const div = document.createElement("div");
   div.className = `${row}-${column}`;
+  if (ship) { div.className += ` ship` }
+  console.log(div);
   return div;
 }
 
 const startGame = () =>{
   const game = new Game();
+
+  //testing purpouses only
+  const testShip = new Ship(4, "v");
+  game.players[0].gameboard.placeShip(["C", 4], testShip);
+
   document.body.appendChild(loadBoard(game.players[0].gameboard));
   document.body.appendChild(loadBoard(game.players[1].gameboard));
 };
